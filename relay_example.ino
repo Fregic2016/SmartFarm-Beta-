3113745a7d2d8bfd6fc54f5209c4e6ffb53a6a21
DongJunKim <dongjun6288@naver.com>
2016-10-05 12:46:36 +0900

    Add files via upload

---
+++ relay_example.ino
@@ -0,0 +1,28 @@
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(10, OUTPUT); //10번핀으로 컨트롤
+  
+  
+}
+void loop()
+{
+  int i;
+  if(Serial.available())
+  {
+    i = Serial.read(); // 시리얼포트에서 읽어들이는 값 확인
+  }
+  if(i == 49)
+  {
+  digitalWrite(10,1); // 전류 흘려보냄
+  }
+  if(i== 50){
+  digitalWrite(10,0); // 전류 차단함
+  }
+  Serial.println(i);
+  delay(500);
+  
+  
+}
+
